f718aa082dd6bdeafaa9ac0a49063e71816d12d1
fly <xiangtong.liu@outlook>
2022-08-26 12:29:36 +0800

    jn

--- src/main/java/com/lxt/Hello.java
+++ src/main/java/com/lxt/Hello.java
@@ -3,5 +3,7 @@ package com.lxt;
 public class Hello {
     public static void main(String[] args){
         System.out.println("hh");
+        System.out.println("hh");
+        System.out.println("hh");
     }
 }
